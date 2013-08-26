5d92413bec00ec0d4677db9a89fd0582ab262052
Mark Burns <markthedeveloper@gmail.com>
2013-08-26 17:13:28 +0100

    remove spec as is pretty much duplicated anyway

--- secure_message/spec/functional_spec.rb
+++ secure_message/spec/functional_spec.rb
@@ -9,30 +9,39 @@ describe "Functional" do
   end
 
   let(:recipient_email) { "mark@example.com" }
-  let(:transport) { double 'transport', deliver: true }
+  let(:transport) { SecureMessage::EmailTransport.new }
 
   let(:encrypted_body) do 
     "===BEGIN ENCRYPTED MESSAGE===\nUnencrypted message\n===END ENCRYPTED MESSAGE===\n"
   end
 
-
   before :all do
     SecureMessage::ActiveRecord::UserPersistence.destroy_all
   end
 
-  before do
-    SecureMessage::ActiveRecord::UserPersistence.
-      create({email: recipient_email, pgp_public_key: "some key"})
+  context "happy path" do
+    before do
+      SecureMessage::ActiveRecord::UserPersistence.
+        create({email: recipient_email, pgp_public_key: "some key"})
+    end
+
+    specify do
+      expected = {from: "from",
+                  to: recipient_email,
+                  body: encrypted_body}
+
+
+      Mail::Message.any_instance.should_receive(:deliver!)
+      SecureMessage.deliver message, transport: transport
+    end
   end
 
-  it do
-    expected = {from: "from",
-                to: recipient_email,
-                body: encrypted_body}
+  context "User not found" do
+    let(:recipient_email) { "not-found@example.com" }
 
-
-    transport.should_receive(:deliver).with expected
-
-    SecureMessage.deliver message, transport: transport
+    specify do
+      expect { SecureMessage.deliver message, transport: transport }.
+        to raise_error SecureMessage::UserNotFound
+    end
   end
 end
