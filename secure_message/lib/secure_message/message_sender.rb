3bbec1dafa925146e2211f5a899d1a5bc5b5b023
Mark Burns <markthedeveloper@gmail.com>
2013-08-26 19:32:17 +0100

    I think I prefer state in small objects to passing arguments everywhere

--- secure_message/lib/secure_message/message_sender.rb
+++ secure_message/lib/secure_message/message_sender.rb
@@ -4,8 +4,7 @@ module SecureMessage
       @dao        = options[:dao]       || ActiveRecord::Dao.new
       @transport  = options[:transport] || EmailTransport.new
 
-      encryptor  = recipient(message.to)
-
+      @to_email = message.to
       @message = EncryptedMessage.new(message, encryptor)
     end
 
@@ -15,16 +14,22 @@ module SecureMessage
 
     private
 
-    def recipient(email)
-      Recipient.new persisted_user: persisted_user(email)
+    attr_reader :to_email
+
+    def encryptor
+      recipient
     end
 
-    def persisted_user(email)
-      @persisted_user = @dao.user_for(email) || raise_not_found(email)
+    def recipient
+      Recipient.new persisted_user: persisted_user
     end
 
-    def raise_not_found(email)
-      raise SecureMessage::UserNotFound.new("User for recipient #{email} not found")
+    def persisted_user
+      @persisted_user = @dao.user_for(to_email) || raise_not_found
+    end
+
+    def raise_not_found
+      raise SecureMessage::UserNotFound.new("User for recipient #{to_email} not found")
     end
   end
 end
